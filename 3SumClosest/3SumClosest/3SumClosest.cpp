994f735f57fecbcb50ff03cde9245e008ce7570f
omar-ahmed-sw <58689848+omar-ahmed-sw@users.noreply.github.com>
2023-06-29 14:32:59 +0100

    trying

--- 3SumClosest/3SumClosest/3SumClosest.cpp
+++ 3SumClosest/3SumClosest/3SumClosest.cpp
@@ -11,17 +11,80 @@ int threeSumClosest(vector<int>& nums, int target)
 {
 //sort the vector
 	sort(nums.begin(), nums.end());
-	//identify our 1st positive index
+	bool AllPos = (*nums.begin() >= 0);
+	bool AllNeg = (*(nums.end() - 1) < 0);
+	uint32_t Index = 0U;
+
+
 
 	//identify the index of our target if exists or the closest number to it
+	auto it = find(nums.begin(), nums.end(), target);
+	if (it != nums.end())
+	{
+		Index = it - nums.begin();
+	}
+	else
+	{
+		//if element doesn't exist then let's get the bounds for it
+		auto LowerBound = lower_bound(nums.begin(), nums.end(), target);
+		auto UpperBound = upper_bound(nums.begin(), nums.end(), target);
+		
+		//and pick the one that's closest to the target
+		int newtarget = ((*UpperBound - target) <= (target - *LowerBound)) ? (*UpperBound) : (*LowerBound);
+		Index = find(nums.begin(), nums.end(), newtarget) - nums.begin();
 
-
+	}
+	
 	//add to it the smallest pos & smallest neg or smallest 2 +ve or 2 -ve
+	 int closestvalue = nums[Index];
+	 nums.erase(nums.begin() + Index);
+	//case all positive or all negative
+	if (AllPos || AllNeg)
+	{
+		closestvalue += nums[0];
+		nums.erase(nums.begin());
+		closestvalue += nums[0];
+
+		return closestvalue;
+	}
+	//mix of both
+	else
+	{
+		//identify our 1st positive index
+		uint32_t i = 0U;
+		auto firstpos = lower_bound(nums.begin(), nums.end(), 0);
+		Index = firstpos - nums.begin() - 1;
+		closestvalue += nums[Index];
+		if (closestvalue < target)  //means what we added was -ve
+		{
+			closestvalue += nums[Index+1];
+			return closestvalue;
+		}
+		else //means what we added was 0
+		{
+			//so here we need to decicde which to take is smaller -ve side number or +ve side
+			if (nums[Index + 1] > abs(nums[Index - 1]))
+			{
+				closestvalue += nums[Index - 1];
+			}
+			else
+			{
+				closestvalue += nums[Index + 1];
+			}
+			return closestvalue;
+		}
+	}
 }
 
 int main()
 {
+	vector<int> nums = { 1,2,5,3,6 };
+	int closestvalue = threeSumClosest(nums, 4);
 
+	while (true)
+	{
+
+	}
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
